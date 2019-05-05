1d7c6bd2aaa4f6f389c9e3b360e264eb7c714744
alessanderleite <alessander.logistica@gmail.com>
2019-05-05 16:03:22 -0300

    initial commit

---
+++ src/main/java/com/demo/jersey/JerseyRestfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JerseyRestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseyRestfulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/jersey/JerseyRestfulWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.jersey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JerseyRestfulWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
