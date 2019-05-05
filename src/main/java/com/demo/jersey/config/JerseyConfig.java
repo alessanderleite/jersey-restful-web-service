4c193a09fea39ab1a966e5d414668e21e60da0e5
alessanderleite <alessander.logistica@gmail.com>
2019-05-05 16:58:58 -0300

    add @Component annotation and make the JerseyConfig class extends ResourceConfig

--- src/main/java/com/demo/jersey/config/JerseyConfig.java
+++ src/main/java/com/demo/jersey/config/JerseyConfig.java
@@ -1,5 +1,14 @@
 package com.demo.jersey.config;
 
-public class JerseyConfig {
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
 
+import com.demo.jersey.service.UserRestService;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+
+	public JerseyConfig() {
+		register(UserRestService.class);
+	}
 }
