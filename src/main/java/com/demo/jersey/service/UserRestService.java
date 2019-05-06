e6501b75c615a9345ec90c0297ee2543622c9a37
alessanderleite <alessander.logistica@gmail.com>
2019-05-05 21:56:06 -0300

    add POST, PUT and DELETE annotation

--- src/main/java/com/demo/jersey/service/UserRestService.java
+++ src/main/java/com/demo/jersey/service/UserRestService.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,42 +22,93 @@ import com.demo.jersey.model.User;
 import com.demo.jersey.model.Users;
 
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name = "users")
+@XmlRootElement(name="users")
 @Path("/users")
 public class UserRestService {
-	private static Map<Integer, User> INMEMORYDB = new HashMap<Integer, User>();
+	private static Map<Integer, User> INMEMORYDB = new HashMap<>();
 	static {
 		User user1 = new User();
 		user1.setId(1);
-		user1.setFirstName("Alessander");
-		user1.setLastName("Leite");
-		
+		user1.setFirstName("Pamela");
+		user1.setLastName("Smith");
+
 		User user2 = new User();
 		user2.setId(2);
-		user2.setFirstName("Alexandre");
-		user2.setLastName("Valente");
+		user2.setFirstName("Louis");
+		user2.setLastName("Lopez");
 		
 		INMEMORYDB.put(user1.getId(), user1);
 		INMEMORYDB.put(user2.getId(), user2);
 	}
-	
+
 	@GET
 	@Produces("application/json")
 	public Users getAllUsers() {
 		Users users = new Users();
-		users.setUsers(new ArrayList<User>(INMEMORYDB.values()));
+		users.setUsers(new ArrayList<>(INMEMORYDB.values()));
 		return users;
 	}
-	
+
 	@GET
 	@Path("/user/{userId}")
 	@Produces("application/json")
 	public Response getUserById(@PathParam("userId") int userId) throws URISyntaxException {
 		User user = INMEMORYDB.get(userId);
 		if (user == null) {
-			return Response.status(404).build();			
+			return Response.status(404).build();
 		}
 		return Response.status(200).entity(user).build();
 	}
-	
+
+	@POST
+	@Consumes("application/json")
+	@Path("/user/create")
+	public Response createUser(User user) throws URISyntaxException {
+		if (user.getFirstName()==null||user.getLastName()==null) {
+			return Response.status(400).entity("Please provide all mandatory inputs").build();
+		}
+		user.setId(INMEMORYDB.values().size() + 1);
+		INMEMORYDB.put(user.getId(),user);
+		return Response.status(201).build();
+	}
+
+	@PUT
+	@Path("/update/{userId}")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response updateUser(@PathParam("userId") int userId, User user) throws URISyntaxException {
+		User temp = INMEMORYDB.get(userId);
+		if (user == null) {
+			return Response.status(404).build();
+		}
+		temp.setFirstName(user.getFirstName());
+		temp.setLastName(user.getLastName());
+		INMEMORYDB.put(temp.getId(), temp);
+		return Response.status(200).entity(temp).build();
+	}
+
+	@DELETE
+	@Path("/delete/{userId}")
+	public Response deleteUser(@PathParam("userId") int userId) throws URISyntaxException {
+		User user = INMEMORYDB.get(userId);
+		if (user != null) {
+			INMEMORYDB.remove(user.getId());
+			return Response.status(200).build();
+		}
+		return Response.status(404).build();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
